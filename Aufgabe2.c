b913727e8c4739b8560f667173f5f203780178c8
murmal <33840470+murmal@users.noreply.github.com>
2017-11-21 16:13:57 +0100

    Update Aufgabe2.c

--- Aufgabe2.c
+++ Aufgabe2.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
+#include <stdint.h>
 
-int counter(char *input){
-	
-
-	int counter = 0;
+int16_t counter(char *input) {
 
 
-	while(*input != '\0'){
+	int16_t counter = 0;
+
+
+	while (*input != '\0') {
 
 		counter++;
 		input++;
@@ -15,40 +16,53 @@ int counter(char *input){
 	return counter;
 }
 
-char* extract(char* input){
+char* extract(char* input) {
 
-	int adresse = (int) input;
-	int laengeInput = counter(input);
+	char* adresse = input;
+	int16_t laengeInput = counter(input);
 
-	while (laengeInput != 0){
-		
-		if (*(adresse+laengeInput) == ':'){
-			if (*(input+laengeInput-1) == ':'){
+	while (laengeInput != 0) {
 
-				return (input+2);
+		if ((*(adresse + laengeInput) == ':') && (*(adresse + laengeInput - 1) == ':')) {
+			
 
-				
+				return (adresse + laengeInput + 1);
 
-			}
 		}
-		else{ laengeInput--;}
-
-		
+		else { laengeInput--; }
 	}
 
-	if (laengeInput == 0){
-
+	if (laengeInput == 0) {
 		return input;
-
-
-
+	}
 }
 
 
-int main(){
+	int16_t main() {
 
-	char i[] = "Hallo";
+		char i[] = "Hallo";
+		char q[] = "Hallo::wie::gehts";
+		char a[] = "::";
+		char k[] = "::jemand:zuhause::";
+		char r[] = "Hier:Sollte::das:zuruckgegeben:werden";
+		char t[] = "Hallo::::asdf";
 
-	int x = counter(i);
-	printf("");
-}
+
+
+
+		char* test = extract(i);
+		char* test1 = extract(q);
+		char* test2 = extract(a);
+		char* test3 = extract(k);
+		char* test4 = extract(r);
+		char* test5 = extract(t);
+
+		printf("Eingabe: %s \nAusgabe: %s\n\n", i,  test);
+		printf("Eingabe: %s \nAusgabe: %s\n\n", q, test1);
+		printf("Eingabe: %s \nAusgabe: %s\n\n", a, test2);
+		printf("Eingabe: %s \nAusgabe: %s\n\n", k, test3);
+		printf("Eingabe: %s \nAusgabe: %s\n\n", r, test4);
+		printf("Eingabe: %s \nAusgabe: %s\n\n", t, test5);
+
+		printf("");
+	}
