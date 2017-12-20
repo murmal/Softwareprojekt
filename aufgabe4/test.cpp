fe543181ecdd38537d2691b0550b0b69ffbb3d43
murmal <33840470+murmal@users.noreply.github.com>
2017-12-20 15:37:37 +0100

    Update test.cpp

--- aufgabe4/test.cpp
+++ aufgabe4/test.cpp
@@ -12,19 +12,39 @@ ostream& operator<< (ostream &out, String &s) {
 int main() {
 
 	String s;
-	String s2('H');
-	String s3("hallo");
+
+	String si('a');
+	String sii('s');
+	String s3("wie gehts?");
+	String s33("mir gehts gut danke");
 	String s4(s3);
-	String s5("peeeeeeeenis");
-
+	String s5("hallo ");
+	String s44(s33);
 	s5 += s3;
-	s2 = s5;
+	s4 += s33;
+	String	s2 = s5;
+	s = sii;
 
-	cout << s5 << endl;		//hallo
-	cout << s2 << endl;		//peeeeeenis
-	cout << s3 << endl;		//hallo
-	cout << s4 << endl;		//hallo
-	cout << s4[4];
+	cout << "Strings mit einzelnen Charactern:" << endl;
+	cout << si << endl;
+	cout << sii << endl;
+	cout << "\nStrings mit Zeichenkette:"<< endl;	
+	cout << s3 << endl;		//wie gehts
+	cout << s33 << endl;
+	cout << "\nStrings aus Strings:" << endl;
+	cout << s4 << endl;		//wie gehts
+	cout << s44 << endl;		//wie gehts
+	cout << "\nStrings mit '+=':" << endl;
+	cout << s5 << endl;		//hallo wie gehts?
+	cout << s4 << endl;
+	cout << "\nStrings mit '=':" << endl;
+	cout << s2 << endl;		//hallo
+	cout << s << endl;		//hallo
+	cout << "\nStrings mit '[]':" << endl;
+	cout << s5[3] << endl;
+	cout << s4[4] << endl;
+
+	cout << "ende" << endl;
 
 
 }
